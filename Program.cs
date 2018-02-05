c780ac4e811ee6d480833ade9455782b26a465e7
bezg <36159818+bezg@users.noreply.github.com>
2018-02-05 16:47:52 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("message!");
+            Console.ReadKey();
+        }
+    }
+    
+    private static void hello()
+    {
+      Console.WriteLine("Hello World!");
+    }
+}
